e84fd4697f785cd9186f3fe41449f4a929bc1e3a
pau [itb] marin vilaseca <pau.marin.7e2@itb.cat>
2021-05-07 16:23:15 +0200

    Auotmatic Creation dungeon

---
+++ Assets/Floors/CreateCorridors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCorridors : MonoBehaviour
+{
+
+    public bool corridor = false;
+    BoxCollider boxCollider;
+    public GameObject corridorGameObject;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<GameObject>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!corridor)
+        {
+          GameObject newGameObject =  Instantiate(corridorGameObject, this.transform.position, Quaternion.identity);
+            newGameObject.transform.Rotate(0, this.transform.rotation.y + 90, 0);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        corridor = true;
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Floors/CreateFloors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFloors : MonoBehaviour
+{
+    public bool floor = false;
+    BoxCollider boxCollider;
+    public GameObject floorGameObject;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<GameObject>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!floor)
+        {
+            GameObject newGameObject = Instantiate(floorGameObject, this.transform.position, Quaternion.identity);
+            newGameObject.transform.Rotate(0, this.transform.rotation.y + 90, 0);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        floor = true;
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
